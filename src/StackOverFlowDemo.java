754fff4ed0c40608b4333c35c29c3914143ca46c
zym <598509108@qq.com>
2023-04-19 14:22:45 +0800

    直接内存OOM

--- src/StackOverFlowDemo.java
+++ src/StackOverFlowDemo.java
@@ -1,2 +1,18 @@
 public class StackOverFlowDemo {
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        StackOverFlowDemo demo = new StackOverFlowDemo();
+        try {
+            demo.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:"+demo.stackLength);
+            throw e;
+        }
+    }
 }
